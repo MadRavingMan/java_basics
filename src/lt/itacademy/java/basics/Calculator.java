f74f85a7c54ca88339f686e548267c11a0286417
MadRavingMan <48089175+MadRavingMan@users.noreply.github.com>
2021-02-03 12:55:23 +0200

    update for 11task
    update for 11task

--- src/lt/itacademy/java/basics/Calculator.java
+++ src/lt/itacademy/java/basics/Calculator.java
@@ -3,69 +3,136 @@ package lt.itacademy.java.basics;
 import java.util.Scanner;
 
 public class Calculator {
-    static void calcFigurePerimeterArea(){
+    static void calcFigurePerimeterArea() {
         Scanner scanner = new Scanner(System.in);
+        boolean isLooper = true;
 
-
-        while(true) {
+        while (isLooper) {
 
             System.out.println("Choose figure(Triangle, Rectangle or Square) or type exit");
             String figure = scanner.nextLine().toLowerCase();
-            int a = 0;
-            int b = 0;
-            int c = 0;
+            int aSide = 0;
+            int bSide = 0;
+            int cSide = 0;
             int perimeter = 0;
             int area = 0;
+            boolean isValid = true;
 
             switch (figure) {
                 case "triangle":
-                    System.out.print("enter a side length: ");
-                    a = Integer.parseInt(scanner.nextLine());
-                    System.out.print("enter b side length: ");
-                    b = Integer.parseInt(scanner.nextLine());
-                    System.out.print("enter b side length: ");
-                    c = Integer.parseInt(scanner.nextLine());
+                    while (isValid) {
+                        System.out.print("enter A side length: ");
+                        String firstInput = scanner.nextLine();
+                        if (isNumeric(firstInput)) {
+                            aSide = Integer.parseInt(firstInput);
+                        } else {
+                            System.out.println("triangle Line lenth can't be: " + firstInput);
+                            continue;
+                        }
 
-                    perimeter = a + b + c;
+                        System.out.print("enter B side length: ");
+                        String secondInput = scanner.nextLine();
+                        if (isNumeric(secondInput)) {
+                            bSide = Integer.parseInt(secondInput);
+                        } else {
+                            System.out.println("triangle Line lenth can't be: " + secondInput);
+                            continue;
+                        }
 
-                    double s = perimeter/2.0;
-                    double abs = s * (s - a) * (s - b) * (s - c);
+                        System.out.print("enter C side length: ");
+                        String thirdInput = scanner.nextLine();
+                        if (isNumeric(thirdInput)) {
+                            cSide = Integer.parseInt(thirdInput);
+                        } else {
+                            System.out.println("triangle Line lenth can't be: " + thirdInput);
+                            continue;
+                        }
+
+                        isValid = false;
+
+                    }
+
+                    perimeter = aSide + bSide + cSide;
+
+                    double s = perimeter / 2.0;
+                    double abs = s * (s - aSide) * (s - bSide) * (s - cSide);
                     double Tarea = Math.round(Math.sqrt(abs) * 1000.0) / 1000.0;
 
                     System.out.println("Triangle perimeter is " + perimeter + " and area is " + Tarea + ".");
                     break;
 
                 case "rectangle":
-                    System.out.print("enter a side length: ");
-                    a = Integer.parseInt(scanner.nextLine());
-                    System.out.print("enter b side length: ");
-                    b = Integer.parseInt(scanner.nextLine());
+                    while (isValid) {
+                        System.out.print("enter A side length: ");
+                        String firstInput = scanner.nextLine();
+                        if (isNumeric(firstInput)) {
+                            aSide = Integer.parseInt(firstInput);
+                        } else {
+                            System.out.println("rectangle Line lenth can't be: " + firstInput);
+                            continue;
+                        }
 
-                    perimeter = (a + b) * 2;
-                    area = a * b;
+                        System.out.print("enter B side length: ");
+                        String secondInput = scanner.nextLine();
+                        if (isNumeric(secondInput)) {
+                            bSide = Integer.parseInt(secondInput);
+                        } else {
+                            System.out.println("rectangle Line lenth can't be: " + secondInput);
+                            continue;
+                        }
+                        isValid = false;
+                    }
+
+                    perimeter = (aSide + bSide) * 2;
+                    area = aSide * bSide;
 
                     System.out.println("Rectangle perimeter is " + perimeter + " and area is " + area + ".");
                     break;
 
                 case "square":
-                    System.out.print("enter a side length: ");
-                    a = Integer.parseInt(scanner.nextLine());
+                    while (isValid) {
+                        System.out.print("enter side length: ");
+                        String firstInput = scanner.nextLine();
+                        if (isNumeric(firstInput)) {
+                            aSide = Integer.parseInt(firstInput);
+                        } else {
+                            System.out.println("square Line lenth can't be: " + firstInput);
+                            continue;
+                        }
+                        isValid = false;
+                    }
 
-                    perimeter = a * 4;
-                    area = a * a;
+                    perimeter = aSide * 4;
+                    area = aSide * aSide;
 
-                    System.out.println("Rectangle perimeter is " + perimeter + " and area is " + area + ".");
+                    System.out.println("Square perimeter is " + perimeter + " and area is " + area + ".");
                     break;
                 case "exit":
-                    System.exit(0);
+                    isLooper = false;
+//                    System.exit(0);
                     break;
                 default:
-                    System.out.println("Please repeat");
+                    System.out.println("Could not understand command " + figure +". Please repeat.");
             }
 
 
         }
 
+    }
+
+    private static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int numb = Integer.parseInt(strNum);
+            if (numb == 0) {
+                return false;
+            }
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 
 }
